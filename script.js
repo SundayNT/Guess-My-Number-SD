537fe3c6ed54a038ae2fc681de6834ad2efbd51a
SundayNT <azterisk13@gmail.com>
2025-02-09 14:04:58 +0700

    7th commit adit bg color and add feat. highscore number

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,10 +26,15 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-    document.querySelector('body').style.backgroundColor = '#118B50';
+    document.querySelector('body').style.backgroundColor = '#347928';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
 
+    if (score > highscore) {
+      highscore = score
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -64,6 +70,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('body').style.backgroundColor = '#041C32';
   document.querySelector('.number').style.width = '15rem';
 });
+
